929333e1445c312bc583eaaf05dc8b962eab5331
DivaniNL <dylanvannierop@gmail.com>
2024-09-02 12:33:29 +0200

    added basic styling

--- own/assets/js/script.js
+++ own/assets/js/script.js
@@ -1,14 +1,13 @@
 jQuery(document).ready(function ($) {
-    console.log('test')
     $('.nav_wrapper .nav_item').on("click tap", function(){
-        console.log('hallooooo')
-        // var $this = $(this);
-        // let num = $this.data('giver');
-        // $this.addClass("active")
-        // $this.siblings().removeClass('active');
-        // let toshow = $('.one-image-tab[data-receiver='+num+']');
-        // toshow.addClass("active")
-        // toshow.siblings().removeClass('active');
+        var $this = $(this);
+        let num = $this.data('giver');
+        console.log(num);
+        $this.addClass("active")
+        $this.siblings().removeClass('active');
+        let toshow = $('article .wrapper[data-receiver='+num+']');
+        toshow.addClass("active")
+        toshow.siblings().removeClass('active');
     })
 
 });
