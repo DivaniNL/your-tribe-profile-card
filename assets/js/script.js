7033e167d8a7c19526da5ab6321f2c55037737c5
DivaniNL <dylanvannierop@gmail.com>
2024-09-03 16:32:31 +0200

    added content

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,35 @@
 jQuery(document).ready(function ($) {
+    setActiveTab();
+    checkActiveTab();
     $('.nav_wrapper .nav_item').on("click tap", function(){
         var $this = $(this);
         let num = $this.data('giver');
-        console.log(num);
-        $this.addClass("active")
+        $this.toggleClass("active")
         $this.siblings().removeClass('active');
         let toshow = $('.article .wrapper[data-receiver='+num+']');
-        toshow.addClass("active")
+        toshow.toggleClass("active")
         toshow.siblings().removeClass('active');
+        checkActiveTab();
     })
 
 });
+
+function checkActiveTab(){
+    var hasActiveClass = $('.wrapper.active').length > 0;
+    if(hasActiveClass) {
+        $('aside').removeClass('no_tab_shown');
+        $('aside').addClass('collapsed');
+    } else {
+        $('aside').removeClass('collapsed');
+        $('aside').addClass('no_tab_shown');
+    }
+}
+function setActiveTab(){
+
+    var $this = $('.nav_wrapper .nav_item').first();
+    let num = $this.data('giver');
+    $this.toggleClass("active")
+    let toshow = $('.article .wrapper[data-receiver='+num+']');
+    toshow.toggleClass("active")
+
+}
